c9445dfc447ec200b713bda7118ccd4d3bb5df8f
Ankur2024 <ag3942280@gmail.com>
2025-05-18 08:38:13 +0530

    file structure done

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -2,8 +2,8 @@ import { Router } from 'express';
 const router = Router();
 import { handleLogin, handleSignup, handleLogout } from "../controller/auth.controller.js"
 
-router.get('/login', handleLogin);
-router.get('/signup', handleSignup);
-router.get('/logout', handleLogout);
+router.post('/login', handleLogin);
+router.post('/signup', handleSignup);
+router.post('/logout', handleLogout);
 
 export default router;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from "dotenv";
-
+import authRoutes from "../routes/auth.routes.js"
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
-// app.use('/api/auth', auth);
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running at port ${PORT}`);
