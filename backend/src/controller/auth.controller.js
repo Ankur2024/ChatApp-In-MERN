4a5a0df1f99f4090ae8c179a8e1140570d6e02f0
Ankur2024 <ag3942280@gmail.com>
2025-05-20 08:30:52 +0530

    Login, logout and stream user creation is done

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,9 +1,39 @@
+import { upsertStreamUser } from "../lib/stream.js";
 import User from "../model/User.model.js";
 import jwt from "jsonwebtoken";
 
 export const handleLogin = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({ message: "Please fill all the fields" });
+        }
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).json({ message: "Invalid credentials" });
+        }
+        const isPasswordCorrect = await user.matchPassword(password);
+        if(!isPasswordCorrect){
+            return res.status(401).json({ message: "Invalid credentials" });
+        }
+        const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET_KEY, {
+            expiresIn: "7d"
+        })
 
+        res.cookie("jwt", token, {
+            maxAge: 7 * 24 * 60 * 1000,
+            httpOnly: true, // prevent XSS attacks
+            sameSite: "strict", // prevent CSRF attacks
+            secure: process.env.NODE_ENV === 'production'
+        })
+        res.status(200).json({ success: true, user});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Error in login controller" });
+    }
 }
+
+
 export const handleSignup = async (req, res) => {
     const { email, password, fullName } = req.body;
     try {
@@ -33,7 +63,16 @@ export const handleSignup = async (req, res) => {
             profilePicture:  randomAvatar,
         })
 
-        // TODO: Create the user in stream as well
+        try {
+            await upsertStreamUser({
+                id: newUser._id.toString(),
+                name: newUser.fullName,
+                image: newUser.profilePicture || ""
+            })
+            console.log("Stream User created for ", newUser.fullName);
+        } catch (error) {
+            console.log("Error in creating stream user", error);
+        }
 
         const token = jwt.sign({userId: newUser._id}, process.env.JWT_SECRET_KEY, {
             expiresIn: "7d"
@@ -55,6 +94,14 @@ export const handleSignup = async (req, res) => {
         res.status(500).json({ message: "Error in signup controller" });
     }
 }
-export const handleLogout = async (req, res) => {
 
+
+export const handleLogout = async (req, res) => {
+    try {
+        res.clearCookie("jwt");
+        res.status(200).json({ message: "Logged out successfully" });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Error in logout controller" });
+    }
 }
