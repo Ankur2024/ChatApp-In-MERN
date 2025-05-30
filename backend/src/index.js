5a6dba6f7de4e5b261db11e92a846a260e31acd0
Ankur2024 <ag3942280@gmail.com>
2025-05-30 10:22:26 +0530

    Chat Routes Creating

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js"
 import userRoutes from "./routes/user.routes.js"
+import chatRoutes from "./routes/chat.routes.js"
 import { connectDb } from './lib/db.js';
 import cookiePareser from "cookie-parser";
 
@@ -17,6 +18,7 @@ app.use(cookiePareser());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/chat', chatRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running at port ${PORT}`);
