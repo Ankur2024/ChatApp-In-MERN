46fdf47e788636588ff0557fe41d2ad84cbab004
Ankur2024 <ag3942280@gmail.com>
2025-06-03 10:58:18 +0530

    Signup UI and backend integration is done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Routes ,Route } from 'react-router'
+import { Routes ,Route, Navigate } from 'react-router'
 import HomePage from './pages/HomePage'
 import SignupPage from './pages/SignupPage'
 import LoginPage from './pages/LoginPage'
@@ -11,25 +11,37 @@ import OnBoardingPage from './pages/OnBoardingPage'
 import ChatPage from './pages/ChatPage'
 import CallPage from './pages/CallPage'
 import { Toaster } from 'react-hot-toast'
+import { axiosInstance } from './lib/axios'
+import { useQuery } from "@tanstack/react-query"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const { data:authData, isLoading, error } = useQuery({
+    queryKey: ["authUser"],
+    queryFn: async () => {
+      const res =await axiosInstance.get("/auth/me");
+      return res.data;
+    },
+    retry: false,
+  });
+
+  const authUser = authData?.user;
 
   return (
-    <div className='h-screen' data-theme="night">
+    <div className="h-screen" data-theme="night">
       <Routes>
-        <Route path='/' element={<HomePage />}/>
-        <Route path='/signup' element={<SignupPage />}/>
-        <Route path='/login' element={<LoginPage />}/>
-        <Route path='/notifications' element={<NotifcationPage />}/>
-        <Route path='/onboarding' element={<OnBoardingPage />}/>
-        <Route path='/chat' element={<ChatPage />}/>
-        <Route path='/call' element={<CallPage />}/>
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/login" element={!authUser ? <LoginPage />: <Navigate to="/" />} />
+        <Route path="/signup" element={!authUser ? <SignupPage />: <Navigate to="/" />} />
+        <Route path="/notification" element={authUser ? <NotifcationPage />: <Navigate to="/login" />} />
+        <Route path="/call" element={authUser? <CallPage />: <Navigate to="/login" />} />
+        <Route path="/chat" element={authUser ? <ChatPage />: <Navigate to="/login" />} />
+        <Route path="/onboarding" element={authUser ? <OnBoardingPage /> : <Navigate to="/login" />} />
       </Routes>
       <Toaster />
-
+      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
